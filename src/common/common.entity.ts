80aa20b4acf1360b22402c8a57ddae48368e6d37
Kwon <anstjdrnjs3@gmail.com>
2022-08-24 23:48:39 +0900

    common entity

---
+++ src/common/common.entity.ts
@@ -0,0 +1,28 @@
+import { Exclude } from 'class-transformer';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class CommonEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updateAt: Date;
+
+  @Exclude()
+  @DeleteDateColumn({
+    type: 'timestamp',
+  })
+  deletedAt?: Date | null;
+}
