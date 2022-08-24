4c7604de7430e220d903d81de0c5250ec796be2c
Kwon <anstjdrnjs3@gmail.com>
2022-08-24 23:36:30 +0900

    User, boards, reviews module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BoardsModule } from './boards/boards.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
+    BoardsModule,
+    ReviewsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
