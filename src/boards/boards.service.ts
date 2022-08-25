5179ab552b6db34fa4bda3e3ebe40505e14f0e0a
Kwon <anstjdrnjs3@gmail.com>
2022-08-25 16:37:05 +0900

    use trasaction

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,21 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { UserEntity } from 'src/users/users.entity';
+import { Repository, DataSource } from 'typeorm';
+
 import { BoardEntity } from './boards.entity';
+import { BoardAddDto } from './dto/boards.add.dto';
 
 @Injectable()
 export class BoardsService {
   constructor(
     @InjectRepository(BoardEntity)
     private readonly boardsRepository: Repository<BoardEntity>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async getSpots() {
-    const boards = await this.boardsRepository.find();
-    return boards;
+    try {
+      return await this.boardsRepository.find();
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
-  async addBoard(boardEntity: BoardEntity): Promise<BoardEntity> {
-    return await this.boardsRepository.save(boardEntity);
+  async addBoard(boardAddDto: BoardAddDto): Promise<BoardEntity> {
+    const { id, title, address, lat, lng, img, discovery } = boardAddDto;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const author = await queryRunner.manager
+        .getRepository(UserEntity)
+        .findOne({
+          where: { id },
+          relations: ['boards'],
+        });
+      const board = queryRunner.manager
+        .getRepository(BoardEntity)
+        .create({ title, address, lat, lng, img, discovery });
+      author.boards.push(board);
+      await queryRunner.manager.getRepository(UserEntity).save(author);
+      await queryRunner.commitTransaction();
+      return board;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e.message);
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
