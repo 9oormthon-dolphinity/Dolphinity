c9d4e234a84e12de2f1cd7677d7535148f7b9bbd
Kwon <anstjdrnjs3@gmail.com>
2022-08-25 13:56:18 +0900

    add, get boards

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { ApiConsumes, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SuccessInterceptor } from 'src/common/success.interceptor';
+import { BoardEntity } from './boards.entity';
+import { BoardsService } from './boards.service';
 
-@Controller('boards')
-export class BoardsController {}
+@Controller('api/boards')
+@UseInterceptors(SuccessInterceptor)
+@ApiTags('BOARD')
+export class BoardsController {
+  constructor(private readonly boardsService: BoardsService) {}
+
+  @ApiConsumes('application/x-www-form-urlencoded')
+  @ApiResponse({
+    status: 200,
+    type: [BoardEntity],
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  @Get()
+  async getSpot(): Promise<BoardEntity[]> {
+    return await this.boardsService.getSpots();
+  }
+
+  @ApiConsumes('application/x-www-form-urlencoded')
+  @ApiResponse({
+    status: 201,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  @Post('register')
+  async addBoard(@Body() boardEntity: BoardEntity): Promise<BoardEntity> {
+    return await this.boardsService.addBoard(boardEntity);
+  }
+}
