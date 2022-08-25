7d07ff87333faaca2a1a30b0bf583990ac23a66a
Kwon <anstjdrnjs3@gmail.com>
2022-08-25 16:37:13 +0900

    board add dto

---
+++ src/boards/dto/boards.add.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
+import { BoardEntity } from '../boards.entity';
+
+export class BoardAddDto extends PickType(BoardEntity, [
+  'title',
+  'address',
+  'lat',
+  'lng',
+  'img',
+  'discovery',
+]) {
+  @ApiProperty({
+    example: 5,
+    description: '사용자 id',
+  })
+  @IsNumber()
+  @Type(() => Number)
+  id: number;
+}
