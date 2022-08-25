0d62c9adcf22605e9d9b7c35c5d5113615016c5c
Kwon <anstjdrnjs3@gmail.com>
2022-08-25 13:55:27 +0900

    setting swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception.filter';
 
@@ -14,6 +15,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('dolphinity')
+    .setDescription('The dolphinity API description')
+    .setVersion('1.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(PORT);
 }
 bootstrap();
