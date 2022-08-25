2e6df81d2819c9b4570a830f06ea94bce057c2e4
Kwon <anstjdrnjs3@gmail.com>
2022-08-26 00:05:10 +0900

    review module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewEntity } from './reviews.entity';
+import { BoardEntity } from 'src/boards/boards.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReviewEntity, BoardEntity])],
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
 })
 export class ReviewsModule {}
