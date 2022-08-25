a385325fed31a8f4e13f29e053599664b3ab2256
Kwon <anstjdrnjs3@gmail.com>
2022-08-26 00:04:58 +0900

    add review controller and service

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiConsumes, ApiResponse } from '@nestjs/swagger';
+import { AddReviewDto } from './dto/review.add.dto';
+import { ReviewsService } from './reviews.service';
 
-@Controller('reviews')
-export class ReviewsController {}
+@Controller('api/reviews')
+export class ReviewsController {
+  constructor(private readonly reviewsService: ReviewsService) {}
+
+  @ApiConsumes('application/x-www-form-urlencoded')
+  @ApiResponse({
+    status: 201,
+    description: '게시물 댓글 API',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  @Post('add')
+  async addReview(@Body() addReviewDto: AddReviewDto) {
+    return this.reviewsService.addReview(addReviewDto);
+  }
+}
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardEntity } from 'src/boards/boards.entity';
+import { DetailBoardDto } from 'src/boards/dto/boards.detail.dto';
+import { UserEntity } from 'src/users/users.entity';
+import { DataSource, Repository } from 'typeorm';
+import { AddReviewDto } from './dto/review.add.dto';
+import { ReviewEntity } from './reviews.entity';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    @InjectRepository(ReviewEntity)
+    private readonly reviewsRepository: Repository<ReviewEntity>,
+    @InjectRepository(BoardEntity)
+    private readonly boardsRepository: Repository<BoardEntity>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async addReview(addReviewDto: AddReviewDto) {
+    const { id, content } = addReviewDto;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const author = await queryRunner.manager
+        .getRepository(UserEntity)
+        .findOne({ where: { id: 1 }, relations: ['boards', 'reviews'] });
+
+      const board = await queryRunner.manager
+        .getRepository(BoardEntity)
+        .findOne({
+          where: { id },
+          relations: ['reviews'],
+        });
+
+      const review = queryRunner.manager
+        .getRepository(ReviewEntity)
+        .create({ content });
+      author.boards.push(board);
+      board.reviews.push(review);
+      author.reviews.push(review);
+      Promise.all([
+        await queryRunner.manager.getRepository(BoardEntity).save(board),
+        await queryRunner.manager.getRepository(UserEntity).save(author),
+      ]);
+      await queryRunner.commitTransaction();
+      return review;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e.message);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
